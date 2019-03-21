397ae966ed6fcb3624f0a4bd0b005dc07ebeb6c7
matheus-eyng <48562512+matheus-eyng@users.noreply.github.com>
2019-03-21 17:43:50 -0300

    Novos metodos

--- PessoaFisica.java
+++ PessoaFisica.java
@@ -1,10 +1,18 @@
-public Class PessoaFisica extends Cliente{
+public class PessoaFisica extends Cliente{
   private int cpf;
   private String estadoCivil;
   
-  public class PessoaFisica(String nome, String endereco, int telefone, String email, int cpf, String estadoCivil){
+  public PessoaFisica(String nome, String endereco, int telefone, String email, int cpf, String estadoCivil){
     super(nome, endereco, telefone, email);
     this.cpf = cpf;
     this.estadoCivil = estadoCivil;
+  }
+
+  public int getCpf() {
+    return this.cpf;
+  }
+
+  public String getEstadoCivil() {
+    return this.estadoCivil;
   }
 }
