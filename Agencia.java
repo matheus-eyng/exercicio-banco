a3514b9b0ef169925e46fa7877ca3e032f273381
matheus-eyng <48562512+matheus-eyng@users.noreply.github.com>
2019-03-21 17:36:37 -0300

    Update Agencia.java
    corrigido o construtor, adicionado metodos get

--- Agencia.java
+++ Agencia.java
@@ -1,18 +1,42 @@
+import java.util.ArrayList;
+
 public class Agencia{
   protected int numero;
   protected String nome;
   protected String endereco;
   protected ArrayList<Clientes> listaClientesA;
   protected ArrayList<Conta> listaContasA;
-  protected static int numeroClientesA;
-  protected static int numeroContasA;
   
-  
-  public class Agencia(int numero, String nome, String endereco){
+  public Agencia(int numero, String nome, String endereco){
      listaContasA = new ArrayList<Conta>();
      listaClientes = new ArrayList<Cliente>();
      this.numero = numero;
      this.nome = nome;
      this.endereco = endereco;
   }
+
+  public int getNumero() {
+    return this.numero;
+  }
+
+  public String getEnderco() {
+    return this.endereco;
+  }
+
+  public ArrayList getClientes() {
+    return this.listaClientesA;
+  }
+
+  public ArrayList getContas() {
+    return this.listaContasA;
+  }
+
+  public void adicionarCliente(Clientes cliente) {
+    listaClientesA.add(cliente);
+  }
+
+  public void adicionarConta(Conta conta) {
+    listaContasA.add(conta);
+  }
+
 }
